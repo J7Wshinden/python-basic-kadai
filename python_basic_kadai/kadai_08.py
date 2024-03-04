55b7f5957f2ddbc40a4dd7a69bfdaccf21f8ad0e
J7Wshinden <160446879+J7Wshinden@users.noreply.github.com>
2024-03-04 16:17:11 +0900

    Update kadai_08.py

--- python_basic_kadai/kadai_08.py
+++ python_basic_kadai/kadai_08.py
@@ -1,12 +1,10 @@
-var=3
+var=15
 
-if var%3==0:
+if var%3==0 & var%5!=0:
     print('Fizz')
-elif var%5==0:
+elif var%5==0 & var%3!=0:
     print('Buzz')
 elif var%15==0:
     print('FizzBuzz')
 else:
     print(var)
-
-print(var)
