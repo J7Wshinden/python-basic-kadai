9c9820f8fc0a588b4f605f3b90a9845239d80233
J7Wshinden <160446879+J7Wshinden@users.noreply.github.com>
2024-03-05 15:17:47 +0900

    Update kadai_011.py

--- python_basic_kadai/kadai_011.py
+++ python_basic_kadai/kadai_011.py
@@ -1,4 +1,12 @@
 arrays = ["水","金","地","火","木","土","天","海","冥"]
+
+for array in arrays:
+ print(array)
+
+for array in arrays:
+  print(f' {array}')
+
+arrays = ["水","金","地","火","木","土","天","海","冥"]
 i = 0
 
 while i < 2:
