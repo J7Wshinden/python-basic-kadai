70d262aeeff275eb0e52986a0e9d4c947a2d6611
J7Wshinden <160446879+J7Wshinden@users.noreply.github.com>
2024-03-06 19:40:18 +0900

    Update kadai_011.py

--- python_basic_kadai/kadai_011.py
+++ python_basic_kadai/kadai_011.py
@@ -1,3 +1,17 @@
+#While
+arrays = ["水","金","地","火","木","土","天","海","冥"]
+i = 0
+j = 0
+
+while i < 9:
+  print(arrays[i])
+  i += 1
+
+while j < 9:
+  print(f' {arrays[j]}')
+  j += 1
+
+#For
 arrays = ["水","金","地","火","木","土","天","海","冥"]
 
 for array in arrays:
@@ -5,13 +19,3 @@ for array in arrays:
 
 for array in arrays:
   print(f' {array}')
-
-arrays = ["水","金","地","火","木","土","天","海","冥"]
-i = 0
-
-while i < 2:
-  i += 1
-
-  for array in arrays:
-    print(array)
-    array = ' ' + array
